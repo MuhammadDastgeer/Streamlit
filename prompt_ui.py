d65fe9064a4c433e5c51fef29187a8ad3ffc290e
MuhammadDastgeer <dastgeerjutt8888@gmail.com>
2025-04-24 18:12:05 +0500

    change

--- prompt_ui.py
+++ prompt_ui.py
@@ -12,7 +12,7 @@ st.header('Reasearch Tool')
 
 # paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
-paper_input = st.text_input("Enter your name:")
+paper_input = st.text_input("Ask Anything:")
 style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
